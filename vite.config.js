40240311f0a7f1b7468b741ed6e2e347ac826229
Michael Luu <67599521+kemonprogrammer@users.noreply.github.com>
2026-08-02 16:03:14 +0200

    install and setup vite-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   // Uses the repo base path in GitHub Actions, falls back to '/' locally
   base: process.env.GITHUB_ACTIONS ? '/pace-converter/' : '/',
   plugins: [
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Pace Converter',
+        short_name: 'PaceConverter',
+        start_url: '/pace-converter',
+        display: 'standalone',
+        background_color: '#0f172b',
+        theme_color: '#616ccc',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 })
